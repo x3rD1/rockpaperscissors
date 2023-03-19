973e564e2f1dd64eb2ad14dc34b103b6278da911
Rexx Alfaro <rexxalfaro+github@gmail.com>
2023-03-18 18:33:13 -0700

    Create a function for computer's choice

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 // prompt a user whether rock paper or scissors
 let playerChoice = prompt('Rock Paper or Scissors?');
 
+//Create a function for computer's choice
+function getComputerChoice(){
+    let choices = ['rock','paper','scissors'];
+    let compChoice = Math.floor(Math.random() * choices.length);
+    
+    if (compChoice === 0){
+        return 'rock';
+    }
+    else if (compChoice === 1){
+        return 'paper';
+    }
+    else if (compChoice === 2){
+        return 'scissors';
+    }
+    return;
+}
+
+
+
