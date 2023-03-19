71df41d7b66c4095ff43ec167ad246235395e4b8
Rexx Alfaro <rexxalfaro+github@gmail.com>
2023-03-18 18:50:06 -0700

    Create a function that compares player's choice and computer's choice

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // prompt a user whether rock paper or scissors
 let playerChoice = prompt('Rock Paper or Scissors?');
 
+// Create a variable for computer's choice
+let computerChoice = getComputerChoice();
+
 //Create a function for computer's choice
 function getComputerChoice(){
     let choices = ['rock','paper','scissors'];
@@ -18,5 +21,29 @@ function getComputerChoice(){
     return;
 }
 
-
-
+// Create a function that compares player's choice and computer's choice
+function compareChoices(playerChoice,computerChoice){
+    if (playerChoice === computerChoice){
+        return 'Draw';
+    }
+    else if (playerChoice === 'rock' && computerChoice === 'paper'){
+        return 'You lose!'
+    }
+    else if (playerChoice === 'rock' && computerChoice === 'scissors'){
+        return 'You win!'
+    }
+    else if (playerChoice === 'paper' && computerChoice === 'rock'){
+        return 'You win!'
+    }
+    else if (playerChoice === 'paper' && computerChoice === 'scissors'){
+        return 'You lose!'
+    }
+    else if (playerChoice === 'scissors' && computerChoice === 'rock'){
+        return 'You lose!'
+    }
+    else if (playerChoice === 'scissors' && computerChoice === 'paper'){
+        return 'You win!'
+    }
+}
+//This tells you if you win or lose
+console.log(compareChoices(playerChoice,computerChoice));
