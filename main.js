5d9452bcd4af6daaf911625599aaa532a6a38600
Rexx Alfaro <rexxalfaro+github@gmail.com>
2023-03-18 19:24:18 -0700

    Create a function called game

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-// prompt a user whether rock paper or scissors
-let playerChoice = prompt('Rock Paper or Scissors?');
+// Create a variable for player's choice which will be prompted soon
+let playerChoice;
 
-// Create a variable for computer's choice
-let computerChoice = getComputerChoice();
+//Create a score board
+let playerScore = 0;
+let computerScore = 0;
 
 //Create a function for computer's choice
 function getComputerChoice(){
@@ -19,7 +20,7 @@ function getComputerChoice(){
         return 'scissors';
     }
     return;
-}
+}   
 
 // Create a function that compares player's choice and computer's choice
 function compareChoices(playerChoice,computerChoice){
@@ -27,23 +28,49 @@ function compareChoices(playerChoice,computerChoice){
         return 'Draw';
     }
     else if (playerChoice === 'rock' && computerChoice === 'paper'){
+        computerScore++
         return 'You lose!'
     }
     else if (playerChoice === 'rock' && computerChoice === 'scissors'){
+        playerScore++
         return 'You win!'
     }
     else if (playerChoice === 'paper' && computerChoice === 'rock'){
+        playerScore++
         return 'You win!'
     }
     else if (playerChoice === 'paper' && computerChoice === 'scissors'){
+        computerScore++
         return 'You lose!'
     }
     else if (playerChoice === 'scissors' && computerChoice === 'rock'){
+        computerScore++
         return 'You lose!'
     }
     else if (playerChoice === 'scissors' && computerChoice === 'paper'){
+        playerScore++
         return 'You win!'
     }
 }
-//This tells you if you win or lose
-console.log(compareChoices(playerChoice,computerChoice));
+
+//Create a function called game
+function game(){
+    for (let i = 0; i < 5; i++){
+        //This allows the user to be prompted 5 times
+        playerChoice = prompt('Rock Paper or Scissors?');
+        //This tells you if you win or lose the round
+        console.log(compareChoices(playerChoice,getComputerChoice()));
+    }
+
+    if (playerScore > computerScore){
+        console.log('You win the game!');
+    }
+    else if (playerScore < computerScore){
+        console.log('You lose the game!');
+    }
+    else{
+        console.log('Draw!');
+    }
+}
+
+game();
