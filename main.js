4ec730d88d234108d5eb80c8708ae5187fd27837
Rexx Alfaro <rexxalfaro+github@gmail.com>
2023-04-03 19:12:21 -0700

    Complete rps game

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let computerScore = 0;
 
 //Create a function for computer's choice
 function getComputerChoice(){
-    let choices = ['rock','paper','scissors'];
+    let choices = ['Rock','Paper','Scissors'];
     let compChoice = Math.floor(Math.random() * choices.length);
     
     if (compChoice === 0){
@@ -25,31 +25,31 @@ function getComputerChoice(){
 // Create a function that compares player's choice and computer's choice
 function compareChoices(playerChoice,computerChoice){
     if (playerChoice === computerChoice){
-        return 'Draw';
+        return `Draw! Both chose ${playerChoice}`;
     }
-    else if (playerChoice === 'rock' && computerChoice === 'paper'){
+    else if (playerChoice === 'Rock' && computerChoice === 'Paper'){
         computerScore++
-        return 'You lose!'
+        return 'You lose! Computer chose Paper.'
     }
-    else if (playerChoice === 'rock' && computerChoice === 'scissors'){
+    else if (playerChoice === 'Rock' && computerChoice === 'Scissors'){
         playerScore++
-        return 'You win!'
+        return 'You win! Computer chose Scissors.'
     }
-    else if (playerChoice === 'paper' && computerChoice === 'rock'){
+    else if (playerChoice === 'Paper' && computerChoice === 'Rock'){
         playerScore++
-        return 'You win!'
+        return 'You win! Computer chose Rock.'
     }
-    else if (playerChoice === 'paper' && computerChoice === 'scissors'){
+    else if (playerChoice === 'Paper' && computerChoice === 'Scissors'){
         computerScore++
-        return 'You lose!'
+        return 'You lose! Computer chose Scissors.'
     }
-    else if (playerChoice === 'scissors' && computerChoice === 'rock'){
+    else if (playerChoice === 'Scissors' && computerChoice === 'Rock'){
         computerScore++
-        return 'You lose!'
+        return 'You lose! Computer chose Rock.'
     }
-    else if (playerChoice === 'scissors' && computerChoice === 'paper'){
+    else if (playerChoice === 'Scissors' && computerChoice === 'Paper'){
         playerScore++
-        return 'You win!'
+        return 'You win! Computer chose Paper.'
     }
     return;
 }
@@ -57,38 +57,84 @@ function compareChoices(playerChoice,computerChoice){
 //Create a function called game
 function game(){
 
-    if (playerScore > computerScore){
-        console.log('You win the game!');
-    }
-    else if (playerScore < computerScore){
-        console.log('You lose the game!');
-    }
-    else{
-        console.log('Draw!');
-    }
+    //add an event listeners to the buttons
+    const buttonOne = document.querySelector('.buttonOne');
+    const buttonTwo = document.querySelector('.buttonTwo');
+    const buttonThree = document.querySelector('.buttonThree');
+
+    //create a score count for both players
+    const container = document.querySelector('body');
+    const score1 = document.querySelector('.pscore');
+    const score2 = document.querySelector('.Cscore');
+
+    //return the result of the round
+    const announce = document.querySelector('.announce');
+    const again = document.createElement('p');
+    again.textContent = 'Press any buttons to play again.'
+
+    //audio
+    const audio = document.querySelector('audio[data-key="bano"]');
+    const wow = document.querySelector('audio[data-key="wow"]');
+
+    buttonOne.addEventListener('click', () => {
+        announce.textContent = compareChoices('Rock', getComputerChoice());
+        score1.textContent = 'Player Score: ' + `${playerScore}`;
+        score2.textContent = 'Computer Score: ' + `${computerScore}`;
+        if (playerScore === 5){
+            announce.textContent = 'Congrats You win!';
+            announce.appendChild(again);
+            playerScore = 0;
+            computerScore = 0;
+            wow.play();
+        }
+        else if (computerScore === 5){
+            announce.textContent = 'You disgust me!';
+            announce.appendChild(again);
+            playerScore = 0;
+            computerScore = 0;
+            audio.play();
+        }
+    });
+    buttonTwo.addEventListener('click', () => {
+        announce.textContent = compareChoices('Paper', getComputerChoice());
+        score1.textContent = 'Player Score: ' + `${playerScore}`;
+        score2.textContent = 'Computer Score: ' + `${computerScore}`;
+        if (playerScore === 5){
+            announce.textContent = 'Congrats You win!';
+            announce.appendChild(again);
+            playerScore = 0;
+            computerScore = 0;
+            wow.play();
+        }
+        else if (computerScore === 5){
+            announce.textContent = 'You disgust me!';
+            announce.appendChild(again);
+            playerScore = 0;
+            computerScore = 0;
+            audio.play();
+        }
+    });
+    buttonThree.addEventListener('click', () => {
+        announce.textContent = compareChoices('Scissors', getComputerChoice());
+        score1.textContent = 'Player Score: ' + `${playerScore}`;
+        score2.textContent = 'Computer Score: ' + `${computerScore}`;
+        if (playerScore === 5){
+            announce.textContent = 'Congrats You win!';
+            announce.appendChild(again);
+            playerScore = 0;
+            computerScore = 0;
+            wow.play();
+        }
+        else if (computerScore === 5){
+            announce.textContent = 'You disgust me!';
+            announce.appendChild(again);
+            playerScore = 0;
+            computerScore = 0;
+            audio.play();
+        }
+    });
+    
+    
+    
 }
-
-
-
-//add an event listeners to the buttons
-
-const buttonOne = document.querySelector('.buttonOne');
-const buttonTwo = document.querySelector('.buttonTwo');
-const buttonThree = document.querySelector('.buttonThree');
-
-buttonOne.addEventListener('click', () => {
-    console.log(compareChoices('rock', getComputerChoice()))
-    console.log(playerScore)
-    console.log(computerScore)
-});
-buttonTwo.addEventListener('click', () => {
-    console.log(compareChoices('paper', getComputerChoice()))
-    console.log(playerScore)
-    console.log(computerScore)
-});
-buttonThree.addEventListener('click', () => {
-    console.log(compareChoices('scissors', getComputerChoice()))
-    console.log(playerScore)
-    console.log(computerScore)
-});
-
+game();
